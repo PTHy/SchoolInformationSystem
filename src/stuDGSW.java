4d420759f7d7317ff36a81aa52f07e6dfa3daaf1
PTHy <30455428+PTHy@users.noreply.github.com>
2018-05-01 16:46:05 +0900

    change something
    선택 >> 뺏음

--- src/stuDGSW.java
+++ src/stuDGSW.java
@@ -10,28 +10,29 @@ public class stuDGSW{
 		Scanner sc = new Scanner(System.in);
 		int sel, sNum;
 		String sName;
-		System.out.print("선택 >>");
-		sel = sc.nextInt();
-		
-		if(sel == 2) {
-			System.out.println("프로그램이 종료되었습니다.");
-			System.exit(0);
-		}else if(sel == 1){
-			System.out.print("학번 : ");
-			sNum = sc.nextInt();
-			System.out.print("이름 : ");
-			sName = sc.next();
-			PersonInfo person = new PersonInfo(sNum, sName);
-			System.out.println("\n");
-			person.ShowPersonInfo();
-		}
+		System.out.print("학번 : ");
+		sNum = sc.nextInt();
+		System.out.print("이름 : ");
+		sName = sc.next();
+		PersonInfo person = new PersonInfo(sNum, sName);
+		System.out.println("\n");
+		person.ShowPersonInfo();
 	}
 	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sel;
 		while(true) {
 			System.out.println("====DGSW 학생정보시스템====");
 			showMenu();
-			readData();
+			System.out.print("선택 >>");
+			sel = sc.nextInt();
+			if(sel == 2) {
+				System.out.println("프로그램이 종료되었습니다.");
+				System.exit(0);
+			}else if(sel == 1){
+				readData();
+			}
 		}
 	}
 }
