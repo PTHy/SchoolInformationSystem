a9b46a4ba7b6907f887e527a007cda32440fa57b
PTHy <30455428+PTHy@users.noreply.github.com>
2018-05-01 16:32:09 +0900

    finish person info and stuDgsw
    maybe it will be a long project cheer up!

--- src/PersonInfo.java
+++ src/PersonInfo.java
@@ -1,4 +1,29 @@
 
 public class PersonInfo {
-
+	int sNum;
+	String sName;
+	public int getsNum() {
+		return sNum;
+	}
+	public void setsNum(int sNum) {
+		this.sNum = sNum;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	
+	public PersonInfo(int sNum,String sName) {
+		this.setsNum(sNum);
+		this.setsName(sName);
+	}
+	
+	public void ShowPersonInfo() {
+		System.out.println("입력된 정보 출력...");
+		System.out.println("학번 : "+this.getsNum());
+		System.out.println("이름 : "+this.getsName());
+		System.out.println("\n");
+	}
 }
